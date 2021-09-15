e86651552e5ac8da0821882c849d28b9fb067365
Jean Victor <jvrferreira96@gmail.com>
2021-09-15 15:50:25 -0300

    FindById and Exceptions

---
+++ src/main/java/com/jvrskt/workshopmongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.jvrskt.workshopmongo.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jvrskt.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), "Not Found", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
---
+++ src/main/java/com/jvrskt/workshopmongo/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.jvrskt.workshopmongo.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
