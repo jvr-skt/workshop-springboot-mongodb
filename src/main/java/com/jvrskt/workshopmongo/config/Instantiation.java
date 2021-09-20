5a589ed8e2e894619e0f003ef70d24ff949647ce
Jean Victor <jvrferreira96@gmail.com>
2021-09-20 14:54:09 -0300

    Author data with DTO

--- src/main/java/com/jvrskt/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jvrskt/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jvrskt.workshopmongo.domain.Post;
 import com.jvrskt.workshopmongo.domain.User;
+import com.jvrskt.workshopmongo.dto.AuthorDTO;
 import com.jvrskt.workshopmongo.repositories.PostRepository;
 import com.jvrskt.workshopmongo.repositories.UserRepository;
 
@@ -36,11 +37,12 @@ public class Instantiation implements CommandLineRunner {
 		User marlene = new User(null, "Marlene Rodrigues", "marlene@gmail.com");
 		User nilson = new User(null, "Nilson Ferreira", "nilson@gmail.com");
 		
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", marlene);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia","Acordei feliz hoje!", marlene);
-		
 		userRepository.saveAll(Arrays.asList(jean, bruna, marlene, nilson));
+
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(marlene));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(marlene));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
--- src/main/java/com/jvrskt/workshopmongo/domain/Post.java
+++ src/main/java/com/jvrskt/workshopmongo/domain/Post.java
@@ -7,6 +7,8 @@ import java.util.Objects;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.jvrskt.workshopmongo.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable{
 
@@ -17,18 +19,18 @@ public class Post implements Serializable{
 	private Date date;
 	private String title;
 	private String body;
-	private User author;
+	private AuthorDTO authorDTO;
 	
 	public Post() {
 	}
 
-	public Post(String id, Date date, String title, String body, User author) {
+	public Post(String id, Date date, String title, String body, AuthorDTO authorDTO) {
 		super();
 		this.id = id;
 		this.date = date;
 		this.title = title;
 		this.body = body;
-		this.author = author;
+		this.authorDTO = authorDTO;
 	}
 
 	public String getId() {
@@ -63,12 +65,12 @@ public class Post implements Serializable{
 		this.body = body;
 	}
 
-	public User getAuthor() {
-		return author;
+	public AuthorDTO getAuthorDTO() {
+		return authorDTO;
 	}
 
-	public void setAuthor(User author) {
-		this.author = author;
+	public void setAuthor(AuthorDTO authorDTO) {
+		this.authorDTO = authorDTO;
 	}
 
 	@Override
